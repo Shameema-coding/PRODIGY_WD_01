3764d5ba671801de3ae7c01c365637a41ee363ae
Shameema-coding <shamamu2912@gmail.com>
2024-06-14 14:22:39 +0530

    Create script.css

---
+++ script.css
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const NavigationMenu = document.querySelector('ul');
+
+hamburger.addEventListener('click', ()=>
+{
+	
+	NavigationMenu.classList.toggle('slide');
+});
